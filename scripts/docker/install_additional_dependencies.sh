7386243cd59a91e39a50a07528ffdd7bb487b62f
solomonjoeykao <solomonjoeykao@gmail.com>
2023-03-13 17:43:06 +0800

    Update install_additional_dependencies.sh

--- scripts/docker/install_additional_dependencies.sh
+++ scripts/docker/install_additional_dependencies.sh
@@ -35,10 +35,10 @@ function install_additional_dependencies() {
         echo
         echo "${COLOR_BLUE}Installing additional dependencies while upgrading to newer dependencies${COLOR_RESET}"
         echo
-        pip install --upgrade --upgrade-strategy eager \
+        pip3 install --upgrade --upgrade-strategy eager \
             ${ADDITIONAL_PYTHON_DEPS} ${EAGER_UPGRADE_ADDITIONAL_REQUIREMENTS}
         # make sure correct PIP version is used
-        pip install --disable-pip-version-check "pip==${AIRFLOW_PIP_VERSION}"
+        pip3 install --disable-pip-version-check "pip==${AIRFLOW_PIP_VERSION}"
         echo
         echo "${COLOR_BLUE}Running 'pip check'${COLOR_RESET}"
         echo
@@ -47,14 +47,14 @@ function install_additional_dependencies() {
         echo
         echo "${COLOR_BLUE}Installing additional dependencies upgrading only if needed${COLOR_RESET}"
         echo
-        pip install --upgrade --upgrade-strategy only-if-needed \
+        pip3 install --upgrade --upgrade-strategy only-if-needed \
             ${ADDITIONAL_PYTHON_DEPS}
         # make sure correct PIP version is used
-        pip install --disable-pip-version-check "pip==${AIRFLOW_PIP_VERSION}"
+        pip3 install --disable-pip-version-check "pip==${AIRFLOW_PIP_VERSION}"
         echo
         echo "${COLOR_BLUE}Running 'pip check'${COLOR_RESET}"
         echo
-        pip check
+        pip3 check
     fi
 }
 
